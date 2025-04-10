ab39e3120c00554367d4f881a5511f7054859730
jalerron <ivannikitin9853@gmail.com>
2025-04-10 17:08:05 +0700

    поправил main

--- bt_users/main.py
+++ bt_users/main.py
@@ -1,15 +1,21 @@
 from fastapi import FastAPI
 import uvicorn
 
+from core.config import settings
 from api import router as api_router
 
 
 main_app = FastAPI()
-main_app.include_router(api_router)
+main_app.include_router(
+    api_router,
+    prefix=settings.api.prefix
+    )
 
 
 if __name__ == "__main__":
     uvicorn.run(
-        "main:main_app", 
+        "main:main_app",
+        host=settings.run.server_host,
+        port=settings.run.server_port, 
         reload = True
     )
