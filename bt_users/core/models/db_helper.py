fb9ccc33a52039f95a2d0df7b18b170c1317ef23
jalerron <ivannikitin9853@gmail.com>
2025-04-11 11:06:50 +0700

    create database helper + async engine

---
+++ bt_users/core/models/db_helper.py
@@ -0,0 +1,20 @@
+from sqlalchemy.ext.asyncio import create_async_engine
+
+
+class DatabaseHelper:
+    def __init__(
+        self,
+        url: str,
+        echo: bool = False,
+        echo_pool: bool = False,
+        max_overflow: int = 10,
+        pool_size: int = 5,
+        ) -> None:
+            
+        self.engine = create_async_engine(
+            url=url,
+            echo=echo,
+            echo_pool=echo_pool,
+            max_overflow=max_overflow,
+            pool_size=pool_size,
+        )
